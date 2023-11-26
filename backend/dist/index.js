264e04b6523b14d1cc94131748d4399171f42e0e
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2023-11-26 11:58:57 +0530

    ...

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,9 +1,10 @@
-import express from "express";
-import { config } from "dotenv";
-config();
-const app = express();
-//middlewares
-app.use(express.json());
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
 //connections and listeners
-app.listen(5000, () => console.log("Server Open"));
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+    .then(() => {
+    app.listen(PORT, () => console.log("Server Open & Connected to Database"));
+})
+    .catch((err) => console.log(err));
 //# sourceMappingURL=index.js.map
