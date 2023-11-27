5948c7f18298001a9b99793eb22403e176c783e2
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2023-11-27 22:30:12 +0530

    ....

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { config } from "dotenv";
+import morgan from "morgan";
 config();
 const app = express();
 //middlewares
 app.use(express.json());
+//remove it in production
+app.use(morgan("dev"));
+app.use("/api/v1");
 export default app;
 //# sourceMappingURL=app.js.map
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import {config} from "dotenv";
-
+import morgan from "morgan";
 config();
 
 const app = express();
 
 //middlewares
-app.use(express.json());
+app.use(express.json()); 
+
+//remove it in production
+app.use(morgan("dev"));
+
+app.use("/api/v1");
 
 export default app;
