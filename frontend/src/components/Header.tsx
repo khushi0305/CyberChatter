ff004ca2f3f2416c4b98e1be594217da8f9ead57
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2024-03-18 19:54:33 +0530

    ....

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,52 @@
 import { AppBar, Toolbar } from '@mui/material'
 import React from 'react'
 import Logo from './shared/Logo'
+import { UseAuth } from '../context/AuthContext'
+import NavigationLink from './shared/NavigationLink'
 
 const Header = () => {
-  return <AppBar sx={{bgcolor: "transparent", position: "static", boxShadow: "none"}}>
+  const auth = UseAuth();
+  return (
+  <AppBar sx={{bgcolor: "transparent", position: "static", boxShadow: "none"}}>
     <Toolbar sx={{display: "flex"}}>
         <Logo/>
+        <div>
+          {auth?.isLoggedIn ? (
+            <>
+            <NavigationLink 
+            bg= "#00fffc" 
+            to='/chat' 
+            text='Go To Chat' 
+            textColor='black'
+            />
+            <NavigationLink 
+            bg='#51538f'
+            textColor='white'
+            to='/'
+            text='logout'
+            onClick={auth.logout}
+            />
+            </>
+          ) : (
+<>
+<NavigationLink 
+            bg= "#00fffc" 
+            to='/login' 
+            text='Login' 
+            textColor='black'
+            />
+            <NavigationLink 
+            bg='#51538f'
+            textColor='white'
+            to='/signup'
+            text='Signup'
+            />
+</>
+          )}
+        </div>
     </Toolbar>
   </AppBar>
+  )
 }
 
 export default Header
