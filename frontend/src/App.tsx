645053ead399385d2f895c414cb3373c239d1089
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2024-03-18 18:05:28 +0530

    ....

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login"
 import Signup from "./pages/Signup"
 import Chat from "./pages/Chat"
 import NotFound from "./pages/NotFound"
+import { UseAuth } from "./context/AuthContext"
 function App() {
 return<main>
   <Header/>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,14 +4,17 @@ import App from './App.tsx'
 import './index.css'
 import { ThemeProvider, createTheme } from '@mui/material'
 import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext.tsx'
 
 const theme = createTheme({typography:{fontFamily:"Roboto Slab, serif", allVariants: {color: "white"},}})
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <AuthProvider>
     <BrowserRouter>
     <ThemeProvider theme= {theme}>
     <App />
     </ThemeProvider>
     </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>,
 )
