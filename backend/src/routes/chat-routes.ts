216e94479d83ca8747911903aebeda39f0c5c36a
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2024-04-02 02:31:07 +0530

    ...

--- backend/src/routes/chat-routes.ts
+++ backend/src/routes/chat-routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
-
+import { verifyToken } from "../utils/token-manager.js";
 const chatRoutes = Router();
+chatRoutes.post("/new", verifyToken)
 
 export default chatRoutes;
