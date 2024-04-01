10f1fd1a6c3fa8259866a0fa950e8095aea5f3e9
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2024-04-02 02:25:20 +0530

    ....

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { getAllUsers, userLogin, userSignup } from "../controllers/user-controllers.js";
 import { signupValidator, validate, loginValidator } from "../utils/validators.js";
+import { verifyToken } from "../utils/token-manager.js";
 
 const userRoutes = Router();
 
 userRoutes.get("/",getAllUsers)
 userRoutes.post("/signup", validate(signupValidator), userSignup)
 userRoutes.post("/login", validate(loginValidator), userLogin)
+userRoutes.get("/auth-status", verifyToken, userLogin)
 export default userRoutes;
